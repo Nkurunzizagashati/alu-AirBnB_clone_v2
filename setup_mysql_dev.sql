cfae899622c86884c08afc648661825b3f59b770
Fab <f.nkurunziz@alustudent.com>
2023-06-13 11:06:22 +0200

    preparing a database

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,11 +1,11 @@
--- create the data base if it doesnt exit
+-- creating a database if it doesn't exists
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
--- Create a new use in the localhost COMMENT
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- create a user 'hbnb_dev'@'localhost' if s/he doesn't exists
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' AUTHORIZED BY 'hbnb_dev_db_pwd';
 
--- Grantt the user hbnb_dev all privileges on the database hbnb_dev_db
+-- granting all previleges on hbnb_dev_db to 'hbnb_dev'@'localhost'
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
--- Grant all Select Privileges on performance_schema to hbnb_dev
+-- granting SELECT previlege on performance_schema to 'hbnb_dev'@'localhost'
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
