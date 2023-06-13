d1d7a2ee90290eb7468f4a7985495568c8d27091
Fab <f.nkurunziz@alustudent.com>
2023-06-13 11:02:17 +0200

    preparing a database

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,11 @@
+-- creating a database if it doesn't exists
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- create a user 'hbnb_dev'@'localhost' if s/he doesn't exists
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' AUTHORIZED BY 'hbnb_dev_db_pwd';
+
+-- granting all previleges on hbnb_dev_db to 'hbnb_dev'@'localhost'
 GRANT ALL PREVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- granting SELECT previlege on performance_schema to 'hbnb_dev'@'localhost'
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
