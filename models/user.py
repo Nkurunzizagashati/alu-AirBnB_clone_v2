598aac60b8fc22e519b883a25d13c4116c054e6a
Fab <f.nkurunziz@alustudent.com>
2023-06-13 19:57:32 +0200

    updated users

--- models/user.py
+++ models/user.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel, Base
+from models.base_model import BaseModel
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-
+Base = declarative_base()
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
